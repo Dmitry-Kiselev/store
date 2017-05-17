b555ab25811191be441e4fe1c69ae32e6888c789
Dmitry-Kiselev <kdem27@gmail.com>
2017-05-17 14:27:15 +0300

    update project to use with bootstrap

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'mptt',
     'solo',
+    'bootstrapform',
 
     'users',
     'conf',
@@ -138,3 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
