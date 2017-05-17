766e7faa15b25334db7c3939d01382e8f42c61ef
Dmitry-Kiselev <kdem27@gmail.com>
2017-05-17 15:11:01 +0300

    specify directory for images

--- catalogue/models.py
+++ catalogue/models.py
@@ -10,9 +10,14 @@ class TimeStampedModel(models.Model):
 
 
 class Category(MPTTModel, TimeStampedModel):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50, unique=True)
     parent = TreeForeignKey('self', null=True, blank=True,
                             related_name='children', db_index=True)
+    image = models.ImageField(upload_to='categories', blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -21,11 +26,12 @@ class Category(MPTTModel, TimeStampedModel):
 class Product(TimeStampedModel):
     name = models.CharField(max_length=50, unique=True)
     product_category = models.ForeignKey(Category, on_delete=models.SET_NULL,
-                                 blank=True, null=True, related_name='products')
+                                         blank=True, null=True,
+                                         related_name='products')
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     num_in_stock = models.IntegerField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to='products', blank=True, null=True)
 
     def __str__(self):
         return self.name
