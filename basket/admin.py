6d9461dc9add5dfd3c5f6e16f3821fefb652dfb4
Dmitry-Kiselev <kdem27@gmail.com>
2017-05-17 16:56:55 +0300

    add basket to admin

--- basket/admin.py
+++ basket/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Basket, Line
+
+
+class LineInlineAdmin(admin.TabularInline):
+    model = Line
+
+
+@admin.register(Basket)
+class BasketAdmin(admin.ModelAdmin):
+    list_display = ['user']
+    inlines = [LineInlineAdmin, ]
