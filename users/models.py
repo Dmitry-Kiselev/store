9acaf077e0d0fffacce623a2c616c0dc18fd1e34
Dmitry-Kiselev <kdem27@gmail.com>
2017-05-17 11:29:59 +0300

    add method to check discounts for user

--- users/models.py
+++ users/models.py
@@ -1,6 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from django.contrib.auth.models import AbstractUser
+from order.models import Discount
+
 
 class User(AbstractUser):
     address = models.CharField(max_length=120, verbose_name='Address')
+
+    def has_discount(self):
+        return self.discounts.get_active_discounts.exists()
