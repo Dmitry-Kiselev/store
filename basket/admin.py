933aa8952fa543310876c1e6c5837cd174160aa9
Dmitry-Kiselev <kdem27@gmail.com>
2017-05-18 12:46:29 +0300

    add Line to admin

--- basket/admin.py
+++ basket/admin.py
@@ -6,6 +6,10 @@ from .models import Basket, Line
 class LineInlineAdmin(admin.TabularInline):
     model = Line
 
+@admin.register(Line)
+class LineAdmin(admin.ModelAdmin):
+    list_display = ['product']
+
 
 @admin.register(Basket)
 class BasketAdmin(admin.ModelAdmin):
