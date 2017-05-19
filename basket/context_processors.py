200760133be86e084501b1d2d3055a5e018487b5
Dmitry-Kiselev <kdem27@gmail.com>
2017-05-19 08:55:13 +0300

    save to cache num products in basket in context processor

--- basket/context_processors.py
+++ basket/context_processors.py
@@ -4,16 +4,21 @@ from redis.exceptions import ConnectionError
 
 def basket_lines_count(request):
     count = None
+    conn_error = False
+    key = request.user.basket.pk
     try:
-        key = request.user.basket.pk
-        count = cache.get('basket_%s' % key)
+        count = cache.get('basket_{}'.format(key))
     except AttributeError:
         count = None
+        conn_error = True
     except ConnectionError:
         count = None
+        conn_error = True
     finally:
         if count is None:
             count = request.user.basket.all_lines().count()
+            if not conn_error:
+                cache.set('basket_{}'.format(key), count, None)
 
     return {
         'basket_lines_count': count or 'empty'}
