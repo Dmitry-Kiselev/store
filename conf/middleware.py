22fb93cd4cc7f9ec1768021ee6ad71702d49fddc
Dmitry-Kiselev <kdem27@gmail.com>
2017-05-22 14:26:28 +0300

    DRY

--- conf/middleware.py
+++ conf/middleware.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 from conf.models import SiteConfig
 
 
-class AdminNotifyMiddleware:
+class BaseMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
@@ -16,6 +16,8 @@ class AdminNotifyMiddleware:
         response = self.get_response(request)
         return response
 
+
+class AdminNotifyMiddleware(BaseMiddleware):
     def process_exception(self, request, exception):
         conf = SiteConfig.get_solo()
         mail_to = conf.admin_email
@@ -26,14 +28,7 @@ class AdminNotifyMiddleware:
         send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [mail_to, ])
 
 
-class LoggingMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        return response
-
+class LoggingMiddleware(BaseMiddleware):
     def process_exception(self, request, exception):
         logger = logging.getLogger(__name__)
         logger.error('{} {}: {}'.format(timezone.now(), str(exception),
