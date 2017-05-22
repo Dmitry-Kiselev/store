12be5e356885c09632a32e2a8d9489d17e0a927e
Dmitry-Kiselev <kdem27@gmail.com>
2017-05-22 11:15:31 +0300

    context processor refactoring

--- basket/context_processors.py
+++ basket/context_processors.py
@@ -3,25 +3,15 @@ from redis.exceptions import ConnectionError
 
 
 def basket_lines_count(request):
-    count = None
-    conn_error = False
-    key = None
     if not request.user.is_authenticated():
         return {}
     try:
         key = request.user.basket.pk
         count = cache.get('basket_{}'.format(key))
-    except AttributeError:
-        count = None
-        conn_error = True
-    except ConnectionError:
-        count = None
-        conn_error = True
-    finally:
-        if count is None:
-            count = request.user.basket.all_lines().count()
-            if not conn_error:
-                cache.set('basket_{}'.format(key), count, None)
+    except (AttributeError, ConnectionError):
+        count = request.user.basket.all_lines().count()
+    else:
+        cache.set('basket_{}'.format(key), count, None)
 
     return {
         'basket_lines_count': count or 'empty'}
