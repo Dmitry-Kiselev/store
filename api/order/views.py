b2d79b2fb34cfbd5801bd228538033e6399dfbf7
Dmitry-Kiselev <kdem27@gmail.com>
2017-05-23 15:40:50 +0300

    add permission check

--- api/order/views.py
+++ api/order/views.py
@@ -1,8 +1,13 @@
 from order.models import Order
 from .serializers import OrderSerializer
 from rest_framework import viewsets
+from .permissions import IsOwner
 
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = (IsOwner,)
+
+    def get_queryset(self):
+        return Order.objects.filter(basket__user=self.request.user)
