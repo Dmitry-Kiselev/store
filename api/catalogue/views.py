eece9a5e8679ddec2ea5df4f5776cc6215929b57
Dmitry-Kiselev <kdem27@gmail.com>
2017-05-23 17:22:21 +0300

    use permissions in catalogue

--- api/catalogue/views.py
+++ api/catalogue/views.py
@@ -4,16 +4,19 @@ from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from .permissions import IsStuffOrReadOnly
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = (IsStuffOrReadOnly,)
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = (IsStuffOrReadOnly,)
 
 
 @api_view(['GET'])
