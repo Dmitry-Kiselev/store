a7eb5da407532fd27965484f11ccce2e7999c029
Dmitry-Kiselev <kdem27@gmail.com>
2017-05-23 17:30:59 +0300

    remove unfinished code from repo

--- api/order/serializers.py
+++ api/order/serializers.py
@@ -4,14 +4,7 @@ from order.models import Order
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    basket = serializers.HyperlinkedRelatedField(
-        read_only=True,
-        view_name='baskets-detail'
-    )
-
-    def save(self):
-        super(OrderSerializer, self).save()
-
+    
     class Meta:
         model = Order
         fields = '__all__'
