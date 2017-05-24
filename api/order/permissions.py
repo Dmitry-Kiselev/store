3a8a9cd11591703cb63627c4fef8c8a9b5505b66
Dmitry-Kiselev <kdem27@gmail.com>
2017-05-24 11:39:08 +0300

    fix basket permission

--- api/order/permissions.py
+++ api/order/permissions.py
@@ -3,4 +3,8 @@ from rest_framework import permissions
 
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.basket.user == request.user
+        if hasattr(obj, 'user'):
+            return obj.user == request.user
+
+        elif hasattr(obj, 'basket'):
+            return obj.basket.user == request.user
